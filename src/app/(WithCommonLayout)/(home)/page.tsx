e5c155f54c9a7e042ac68edf91b055dd2695ee5a
munir <smsirajulmonir@gmail.com>
2025-09-12 12:05:43 +0600

    home category added

--- src/app/(WithCommonLayout)/(home)/page.tsx
+++ src/app/(WithCommonLayout)/(home)/page.tsx
@@ -1,12 +1,15 @@
 "use client"
 
+import Category from "@/components/modules/home/category";
+import HeroSection from "@/components/modules/home/hero/HeroSection";
 import { useUser } from "@/context/UserContext";
 
 const HomePage = () => {
     const user = useUser();
     return (
         <div>
-            <h1>Welcome Home Page</h1>
+            <HeroSection/>
+            <Category/>
         </div>
     );
 };
